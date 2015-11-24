70c3e60fb25273d0c5bfe511925bd92b714bd337
Andrew Hart <Andrew@ProjectDent.com>
2015-11-24 16:17:29 +0000

    adds link label to demo

--- LinkLabelDemo/ViewController.swift
+++ LinkLabelDemo/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        let text = "This is some text, which incidentally includes a link you may find interesting."
+        let fullRange = NSMakeRange(0, (text as NSString).length)
+        let linkRange = (text as NSString).rangeOfString("includes a link")
+        let attributedString = NSMutableAttributedString(string: text)
+        attributedString.addAttribute(NSFontAttributeName, value: UIFont.systemFontOfSize(15), range: fullRange)
+        attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.blackColor(), range: fullRange)
+        attributedString.addAttribute(NSLinkAttributeName, value: NSURL(string: "https://google.com")!, range: linkRange)
+        
+        let label = LinkLabel()
+        label.numberOfLines = 0 // Infinite lines
+        label.attributedText = attributedString
+        label.frame = CGRect(x: 20, y: 20, width: 280, height: 400)
+        self.view.addSubview(label)
     }
 
     override func didReceiveMemoryWarning() {
