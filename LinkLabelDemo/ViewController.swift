00cdd667b02acfc0cd9dfa704755f4e6c19012df
Andrew Hart <Andrew@ProjectDent.com>
2015-11-24 16:40:01 +0000

    updates demo to demonstrate custom link attributes

--- LinkLabelDemo/ViewController.swift
+++ LinkLabelDemo/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LinkLabelInteractionDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +24,22 @@ class ViewController: UIViewController {
         attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.blackColor(), range: fullRange)
         attributedString.addAttribute(NSLinkAttributeName, value: NSURL(string: "https://google.com")!, range: linkRange)
         
+        let linkTextAttributes = [
+            NSUnderlineStyleAttributeName: NSNumber(integer: NSUnderlineStyle.StyleSingle.rawValue),
+            NSForegroundColorAttributeName: UIColor.greenColor()
+        ]
+        
+        let highlightedLinkTextAttributes = [
+            NSUnderlineStyleAttributeName: NSNumber(integer: NSUnderlineStyle.StyleSingle.rawValue),
+            NSForegroundColorAttributeName: UIColor.redColor()
+        ]
+        
         let label = LinkLabel()
         label.numberOfLines = 0 // Infinite lines
         label.attributedText = attributedString
+        label.linkTextAttributes = linkTextAttributes
+        label.highlightedLinkTextAttributes = highlightedLinkTextAttributes
+        label.interactionDelegate = self
         label.frame = CGRect(x: 20, y: 20, width: 280, height: 400)
         self.view.addSubview(label)
     }
@@ -36,6 +49,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: LinkLabelInteractionDelegate
+    
+    func linkLabelDidSelectLink(linkLabel linkLabel: LinkLabel, url: NSURL) {
+        print("did select link: \(url)")
+    }
 }
 
