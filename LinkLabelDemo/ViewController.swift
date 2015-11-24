153eb9654d7801c66f7d687a0dacbaa57388579a
Andrew Hart <Andrew@ProjectDent.com>
2015-11-24 15:57:13 +0000

    adds demo project

---
+++ LinkLabelDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkLabelDemo
+//
+//  Created by Andrew Hart on 24/11/2015.
+//  Copyright © 2015 Project Dent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
