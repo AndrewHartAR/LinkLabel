d0c8915b8e283996ede545686fe7ee4141624221
Levi McCallum <levi@levimccallum.com>
2021-02-23 13:28:49 -0800

    Fix package name in Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Kingfisher",
+    name: "LinkLabel",
     platforms: [.iOS(.v10)],
     products: [
         .library(name: "LinkLabel", targets: ["LinkLabel"])
